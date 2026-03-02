76c0ff3816ad052a2efddd4f7245c0cb8d712920
AidanLovejoy <114533161+AidanLovejoy@users.noreply.github.com>
2026-03-02 18:19:40 +0000

    Mon, Mar 2, 2026, 10:19 AM -08:00

--- script/snake.js
+++ script/snake.js
@@ -1,10 +1,12 @@
 panels = document.querySelectorAll('.empty');
-int = setInterval(runGame, 30);
+int = setInterval(runGame, 300);
 
-head = 25;
-tail = 27;
+head = 1;
+length = 3;
+
+//panels = [];
 
 function runGame() {
     panels[head-1].classList.add("snake");
-    panels[tail-1].classList.add("snake");
+
 }
