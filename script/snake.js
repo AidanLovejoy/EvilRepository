b9ab1c9ccfc382de8b6541ed4d559854ae156bcf
AidanLovejoy <114533161+AidanLovejoy@users.noreply.github.com>
2026-03-03 18:02:06 +0000

    Tue, Mar 3, 2026, 10:02 AM -08:00

--- script/snake.js
+++ script/snake.js
@@ -5,12 +5,12 @@ head = 40;
 length = 3;
 
 direction = 1;
+moved = false;
 
 snakeSpots = [];
 
 function addSnake(nextSpot) {
-    for (let i = length; i >= 0; i--)
-    {
+    for (let i = length; i >= 0; i--) {
         snakeSpots[i] = snakeSpots[i - 1];
     }
     snakeSpots[length + 1] = null;
@@ -18,28 +18,37 @@ function addSnake(nextSpot) {
 }
 
 document.addEventListener('keydown', (event) => {
-
-  if (event.key === 'w') {
-    direction = -10;
-  } else if (event.key === 's') {
-    direction = 10;
-  }else if (event.key === 'a') {
-    direction = -1;
-  }else if (event.key === 'd') {
-    direction = 1;
-  }
-
+    if (moved == false) {
+        moved == true;
+        if (event.key === 'w') {
+            if (direction != 10) {
+                direction = -10;
+            }
+        } else if (event.key === 's') {
+            if (direction != -10) {
+                direction = 10;
+            }
+        } else if (event.key === 'd') {
+            if (direction != -1) {
+                direction = 1;
+            }
+        } else if (event.key === 'a') {
+            if (direction != 1) {
+                direction = -1;
+            }
+        }
+    }
 });
 
 
 function runGame() {
-    panels[head-1].classList.add("snake");
-    addSnake(head-1);
+    panels[head - 1].classList.add("snake");
+    addSnake(head - 1);
 
     head += direction;
 
-    if (snakeSpots[length])
-    {
+    if (snakeSpots[length]) {
         panels[snakeSpots[length]].classList.remove("snake");
     }
+    moved = false;
 }
